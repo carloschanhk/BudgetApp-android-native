3505ac2e215abccdf848ff949ebb8daf8acd7ea5
Carlos Chan <carloschan2215@gmail.com>
2021-06-05 16:57:54 +0800

    refactor: change progressbar drawable name

--- app/src/main/java/com/example/budget/common/CategoryType.kt
+++ app/src/main/java/com/example/budget/common/CategoryType.kt
@@ -3,27 +3,27 @@ package com.example.budget.common
 import com.example.budget.R
 
 enum class CategoryType(val type: String, val icon: Int, val color: Int) {
-    APPARELS("Apparels", R.drawable.ic_casual_t_shirt_24, R.drawable.apparels_progressbar),
+    APPARELS("Apparels", R.drawable.ic_casual_t_shirt_24, R.drawable.progressbar_apparels),
     HOUSING(
         "Housing",
         R.drawable.ic_baseline_home_24,
-        R.drawable.housing_progressbar
+        R.drawable.progressbar_housing
     ),
-    SHOPS("Shops", R.drawable.ic_baseline_shopping_cart_24, R.drawable.shops_progressbar),
+    SHOPS("Shops", R.drawable.ic_baseline_shopping_cart_24, R.drawable.progressbar_shops),
     FOOD(
         "Food",
         R.drawable.ic_baseline_fastfood_24,
-        R.drawable.food_progressbar
+        R.drawable.progressbar_food
     ),
     TRANSIT(
         "Transit",
         R.drawable.ic_baseline_directions_transit_24,
-        R.drawable.transit_progressbar
+        R.drawable.progressbar_transit
     ),
     LEISURE(
         "Leisure",
         R.drawable.ic_baseline_sports_esports_24,
-        R.drawable.leisure_progressbar
+        R.drawable.progressbar_leisure
     ),
-    HEALTH("Health", R.drawable.ic_baseline_favorite_24, R.drawable.health_progressbar)
+    HEALTH("Health", R.drawable.ic_baseline_favorite_24, R.drawable.progressbar_health)
 }
